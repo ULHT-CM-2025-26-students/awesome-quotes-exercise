6b195457d5e4c85ad80bbd3865539baeb4f94d92
mstavares <hmigueltavares@gmail.com>
2026-03-15 14:49:42 +0000

    improvements

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -1,6 +1,6 @@
 import 'package:integration_test/integration_test.dart';
 
-import '../test/widget_test.dart';
+import '../test/teacher/widget_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
